ebcdbf8ff03ce71d7f9edfdb2bc96e101686088d
shahal001 <shahalmoideen747400@gmail.com>
2025-12-02 10:37:15 +0530

    setup-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./view/screens/Home";
+import AboutUs from "./view/screens/AboutUs";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>hello world</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+    </Routes>
   );
 }
 
---
+++ src/view/screens/AboutUs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutUs() {
+  return <div className="bg-red-400">AboutUs</div>;
+}
+
+export default AboutUs;
